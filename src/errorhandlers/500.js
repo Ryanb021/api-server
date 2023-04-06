75274f7b4c2db1e31380494d49b7cb9329e55796
Ryan Bagan <darcknight1980@gmail.com>
2023-04-05 18:43:56 -0700

    wow

--- src/errorhandlers/500.js
+++ src/errorhandlers/500.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = (err, request, response, next) {
+module.exports = (err, request, response, next) => {
 response.status(500).send({
   error: 500,
   route: request.path,
